cbfca557edb6aba204f67107689d30b44fc2c5c8
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-12-20 12:50:31 +0500

    function for printing array and getting mean number

--- exercises/arrayMain.c
+++ exercises/arrayMain.c
@@ -16,6 +16,12 @@ int main(int argc, char **argv) {
   int sum = getSum(arr, n);
   printf("sum of elements = %d\n", sum);
   printArray(arr, n);
+  //function for print array and get mean number
+  double mean=getMean(arr,n);
+  if(mean!= -1){
+    printf("The mean Number is %f",mean);
+  
+  }
 
   free(arr);
 
--- exercises/array_utils.c
+++ exercises/array_utils.c
@@ -61,29 +61,147 @@ void freeTable(int **table, int n) {
 }
 
 double getMean(const int *arr, int size) {
-  //TODO: implement
+  //for address of array
+  if(arr==NULL){
+    printf("Invalid arrays address\n");
+    return -1;
+  }
+  // checking size not find mean of 1 element
+  if(size == 0){
+    printf("Invalid\n");
+    return -1;
+  }
+  double sum =0.0;
+  for(int i=0;i<size;i++){
+    sum+=arr[i];
+  }
+  return sum/size;
 }
+int getMin(const int*arr,int size){
+  if(arr==NULL){
+    printf("invalid array address\n");
+       return -1;
 
-int getMin(const int *arr, int size) {
-  //TODO: implement
+  }
+  //check size not find min
+  if(size==0){
+    printf("Invalid size\n");
+    return -1;
+  }
+  int min=arr[0];
+  for(int i=0;i<size;i++){
+    if(arr[i]<min){
+      min=arr[i];
+    }
+  }
+  return min;
+  
 }
-
-int getIndexOfMin(const int *arr, int size) {
-  //TODO: implement
+//function for index of min num
+int getIndexOfMin(const int*arr,int size){
+  if(arr==NULL){
+    printf("Invalid array\n");
+    return -1;
+  }
+  int min=arr[0];
+  int minIndex=0;
+  for(int i=0;i<size;i++){
+    if(arr[i]<min){
+      min=arr[i];
+      minIndex=i;
+    }
+  }
+  return minIndex;
 }
-
-int getMax(const int *arr, int size) {
-  //TODO: implement
+//function for max num array
+int getMax(const int*arr,int size){
+  if(arr==NULL){
+    printf("invalid arrray");
+    return -1;
+  }
+  int max=0;
+  for(int i=0;i<size;i++){
+    if(arr[i]>max){
+      max=arr[i];
+    }
+  }
+  return max;
 }
+//Functin to find index of max
+int getIndexOfMax(const int*arr,int size){
+  if(arr==NULL){
+    printf("invalid ");
+    return -1;
+  }
+  int max =0;
+  int maxIndex =0;
+  for(int i=0;i<size;i++){
+    if(arr[i]>max){
+      if(arr[i]>max){
+        max=arr[i];
+        maxIndex=i;
+      }
+    }
+    return maxIndex;
+  }
+  //function to filter the num from array as condition
+  int *filterThreshold(const int *arr,int size,int threshold,int *resultSize){
+    if(arr==NULL || resultSize==NULL){
+      printf("Invalid array");
+      return NULL;
+    }
+    // variable fot store increment value for array threshold
+    int count =0;
+    for(int i=0;I<size;i++){
+      //filter size for array
+      if(arr[i]>=threshold){ 
+        count++;
+      }
+    }
+    //dynamic memory allocation
+    int *filterArray=(int*)malloc(count*sizeof(int));
+    //to increment dynamic array
+    int index =0;
+    for(int i=0;i<size;i++){
+      //for filter value equal or greater from threshold store in dynamic arr
+      if (arr[i]>=threshold){
+        filterArray[index++]=arr[i];
 
-int getIndexOfMax(const int *arr, int size) {
-  //TODO: implement
-}
+      }
+      *resultsize=count;
+    }
+    return filterArray;
+  }
 
-int * filterThreshold(const int *arr, int size, int threshold, int *resultSize) {
-  //TODO: implement
-}
+  //Multiplication table for 2D array
+  int **createMultiplicationTable(int n,int m){
+    if(n==0 || m==0){
+      printf("invalid\n");
+      return NULL;
+    }
+    //pointer for store adress row
+    int **table =(int **)malloc(n*sizeof(int*));
+    if(table == NULL){
+      printf("Invalid allocation of memory\n");
+      return NULL;
+    }
+    for(int i=0;i<n;i++){
+      // memory for colum to each row
+      table[i]=(int*)malloc(m*sizeof(int));
+      if(table[i]==NULL){
+        printf("Invalid memory for colums index");
 
-int **createMultiplicationTable(int n, int m) {
-  //TODO: implement
+      }
+
+    
+  }
+    // row & colums multiplication
+    for(int i=0;i<n;i++){
+      for(int j=0;j<m;j++){
+        table[i][j]=(i+1)*(j+1);
+      }
+    }
+    //return array
+    return table;
+  
 }
