01575ff0b58645985badc706ab1cfd35e2932242
bilalmalik101 <144342543+bilalmalik101@users.noreply.github.com>
2023-12-20 18:50:28 +0500

    function to return min of numb and to find min index

--- exercises/arrayMain.c
+++ exercises/arrayMain.c
@@ -22,6 +22,19 @@ int main(int argc, char **argv) {
     printf("The mean Number is %f",mean);
   
   }
+  //Function for return min of numb and chech validaty
+  int min= getMin(arr,n);
+  if(min!=-1){
+    printf("The minimum as %d\n",min);
+
+  }
+  // to find min index
+  int minIndex= getIndexOfMin(arr,n);
+  if(minIndex != -1){
+    printf("The index of min is %d",minIndex);
+
+  }
+  int max = getMax(arr,n);
 
   free(arr);
 
